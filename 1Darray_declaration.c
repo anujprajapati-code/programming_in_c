42b1d3a337c5f9b679dd99e8e067990b6aff2cf2
anujprajapati-code <anuj.invalid.24.7@gmail.com>
2025-11-19 22:29:16 +0530

    Add 1D array value assignment program
    Implement a program to assign values in a 1D array.

---
+++ 1Darray_declaration.c
@@ -0,0 +1,12 @@
+/*****WAP to assign the value in 1D array.*****/
+#include <stdio.h>
+int main()
+{
+    int i;
+    int act[i];
+    for(i=0;i<7;i++){
+        printf("\n\t The number at act[%d] is: ",i);
+        scanf("%d",&act[i]);
+    }
+    return 0;
+}
