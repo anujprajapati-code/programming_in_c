9e9ca8b7f96d83e52b58d08af88016d973c7f94a
anujprajapati-code <anuj.invalid.24.7@gmail.com>
2025-11-20 13:26:43 +0530

    Implement week day switch case program
    This program prompts the user to enter a day number and prints the corresponding weekday.

---
+++ week-day_switch.c
@@ -0,0 +1,44 @@
+/** WAP to print the week day using integer.**/
+#include <stdio.h>
+
+int main() {
+    int day;
+
+    printf("Enter day number (1-7): ");
+    scanf("%d", &day);
+
+    switch(day) {
+        case 1:
+            printf("Monday\n");
+            break;
+
+        case 2:
+            printf("Tuesday\n");
+            break;
+
+        case 3:
+            printf("Wednesday\n");
+            break;
+
+        case 4:
+            printf("Thursday\n");
+            break;
+
+        case 5:
+            printf("Friday\n");
+            break;
+
+        case 6:
+            printf("Saturday\n");
+            break;
+
+        case 7:
+            printf("Sunday\n");
+            break;
+
+        default:
+            printf("Invalid day! Please enter number between 1 and 7.\n");
+    }
+
+    return 0;
+}
